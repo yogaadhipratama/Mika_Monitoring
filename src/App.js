c939adf08f363e5e5c3be7af86cb92f6cce208a4
Yoga Adhipratama <yogaadhi@icloud.com>
2022-09-24 17:19:44 +0700

    add about home notfound pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer"
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/Notfound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar title={"Cari-Github"} />
-        <main className="container mx-auto mx-3 pb-12">Content</main>
+        <main className="container mx-auto mx-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
       <Footer/>
-      </div>
+      </div> 
     </Router>
   );
 }
