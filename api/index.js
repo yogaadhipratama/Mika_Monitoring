8aff62392a856cf406f5951536d922ba64219b59
Yoga Adhipratama <yogaadhi@icloud.com>
2022-10-17 14:06:50 +0700

    add backend

---
+++ api/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const model = require("./models");
+const Role = model.role;
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// });
+
+
+//create connection to db ==============
+
+const db = mysql.createConnection({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+//connect to db ========================
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("database connected");
+});
+
+// model.sequelize.sync({ force: true }).then(() => {
+//   console.log("drop and resync db");
+//   initial();
+// });
+model.sequelize.sync()
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Mika Web Monitoring." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Connected to Backend, runs on ${PORT}`);
+});
+
+app.use(express.json()); //enable json req.body
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
---
+++ api/models/index.js
@@ -0,0 +1,45 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+const config = require("../config/db.config.js");
+require('dotenv').config();
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: config.dialect,
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
